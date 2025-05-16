31a6b711b24da17a809b50c29ce4c40b2fbda1d0
Padmaraj Kurundwade <155999113+PadmarajKurundwade07@users.noreply.github.com>
2025-05-16 18:38:07 +0530

    1b_Array_List_Program1

--- 1b_Array_List_Search.java
+++ 1b_Array_List_Search.java
@@ -1,5 +1,5 @@
-/* Write a java program for getting different colors through ArrayList interface and search whether
-   the color "Red" is available or not ?
+/* Write a java program for getting different colors through ArrayList interface and search whether the 
+   color "Red" is available or not ?
 */
 
 
