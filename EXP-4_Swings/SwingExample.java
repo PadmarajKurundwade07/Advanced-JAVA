6dfe25834cef64f50a9bd352e50d0dfb7194b21e
Padmaraj Kurundwade <155999113+PadmarajKurundwade07@users.noreply.github.com>
2025-05-21 16:09:52 +0530

    Update SwingExample.java

--- EXP-4_Swings/SwingExample.java
+++ EXP-4_Swings/SwingExample.java
@@ -1,6 +1,6 @@
 /*
 Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing
-Programming in Blue color plain font with font size of 32 using Jframe and Jlabel
+Programming in Blue color plain font with font size of 32 using Jframe and Jlabel.
 */
 
 
