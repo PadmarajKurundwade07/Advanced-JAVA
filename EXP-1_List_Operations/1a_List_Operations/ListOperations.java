5a10dfd779d5fc910c1caf9d0cd81feaef68aa9b
Padmaraj Kurundwade <155999113+PadmarajKurundwade07@users.noreply.github.com>
2025-05-21 16:01:23 +0530

    Update ListOperations.java

--- EXP-1_List_Operations/1a_List_Operations/ListOperations.java
+++ EXP-1_List_Operations/1a_List_Operations/ListOperations.java
@@ -4,7 +4,7 @@ perform the following operations with different functions directed as follows;
 1. Adding elements, 2. Adding element at specific index, 3. Adding multiple elements, 4.Accessing elements,
 5. Updating elements, 6. Removing elements, 7. Searching elements, 8. Listsize, 9. Iterating over list, 
 10. Using Iterator, 11. Sorting, 12. Sublist, 13. Clearing the list
-*/
+*/ 
 
 package arrayandlinked;
 import java.util.*;
